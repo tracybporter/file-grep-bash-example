b3ac69c1d8d99d9eab1ff8a229ea517459c120b8
Tracy B Porter <tbporter@gmail.com>
2018-10-31 11:15:57 -0500

    tracy - making sure rows match correctly

--- test-search-no-mocking.sh
+++ test-search-no-mocking.sh
@@ -32,22 +32,19 @@ file1match="f1,3,num1003,more,stuff"
 file2match="f2,2,num2002,more,stuff"
 file3match="f3,1,num3001,more,stuff"
 declare -a expectedMatches=($file1match $file2match $file3match)
-notFoundRows=()
+notFoundCount=0
 
 #expects
 for expectedRow in ${expectedMatches[@]}; do
-    if  grep -Fxq $expectedRow $outputFile
-     then
-        echo "found it"
-     else
-        notFoundRows+=($expectedRow)
+    if  [ $(grep --count $expectedRow $outputFile)  = 0 ]; then
+        ((notFoundCount+=1))
     fi
 done
 
-notFoundSize=${#notFoundRows[@]}
-echo $notFoundSize
-
-if ! [ $notFoundSize = 0 ]; then
-    echo "Failed to find $notFoundSize rows"
-    exit 1
+if [ $notFoundCount -gt 0 ]
+    then
+        echo " FAILED: No match found for $notFoundCount rows"
+        exit 1
+    else
+        echo " Success"
 fi
