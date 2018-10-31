41479306ef801163794809dcef9a76a8acba64c6
Tracy B Porter <tbporter@gmail.com>
2018-10-31 09:50:02 -0500

    tracy - simple single row test works.

--- test-simple-no-mocking.sh
+++ test-simple-no-mocking.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 ### Rather than roll my own, if I was really serious about bash script testing I'd dig further into https://github.com/sstephenson/bats
 
+#setup spec
+sh ./simple.sh > testing-output.txt
+
 printf "\nFound the correct number of rows\n"
 #given
 expectedRowCount=1
 
 #when
-sh ./simple.sh > testing-output.txt
 actualRowCount=(`wc testing-output.txt`)
 
 #then
@@ -15,5 +17,18 @@ then
     echo " Success"
 else
     echo " FAILED: Row count was $actualRowCount not $expectedRowCount"
+    exit 1
+fi
+
+printf "\nFound the correct row\n"
+#given
+row1="f2,2,num2002,more,stuff"
+
+#expects
+if grep -Fxq $row1 testing-output.txt
+then
+    echo " Success"
+else
+    echo " FAILED: Row $row1 was not found"
     exit 1
 fi
