c0b4db710d4e1a9f101289a4e0dfa83a7168ef5c
Tracy B. Porter <tbporter@gmail.com>
2018-10-31 12:22:56 -0500

    Assert contents of results (#3)
    * tracy - simple single row test works.
    
    * tracy - wip
    
    * tracy - search test working but ugly
    
    * tracy - making sure rows match correctly

--- test-search-no-mocking.sh
+++ test-search-no-mocking.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 ### Rather than roll my own, if I was really serious about bash script testing I'd dig further into https://github.com/sstephenson/bats
 
+printf "\n\nTESTING search-all-data-files.sh\n"
+
+#setup spec
+outputFile=testing-search-all-output.txt
+rm -f $outputFile
+sh ./search-all-data-files.sh > $outputFile
+
+# --------------------
 printf "\nFound the correct number of rows\n"
 #given
 expectedRowCount=3
 
 #when
-sh ./search-all-data-files.sh > testing-output.txt
-actualRowCount=(`wc testing-output.txt`)
+actualRowCount=(`wc $outputFile`)
 
 #then
 if [ $actualRowCount = $expectedRowCount ]
@@ -16,4 +23,28 @@ then
 else
     echo " FAILED: Row count was $actualRowCount not $expectedRowCount"
     exit 1
+fi
+
+# --------------------
+printf "\nFound the correct rows\n"
+#given
+file1match="f1,3,num1003,more,stuff"
+file2match="f2,2,num2002,more,stuff"
+file3match="f3,1,num3001,more,stuff"
+declare -a expectedMatches=($file1match $file2match $file3match)
+notFoundCount=0
+
+#expects
+for expectedRow in ${expectedMatches[@]}; do
+    if  [ $(grep --count $expectedRow $outputFile)  = 0 ]; then
+        ((notFoundCount+=1))
+    fi
+done
+
+if [ $notFoundCount -gt 0 ]
+    then
+        echo " FAILED: No match found for $notFoundCount rows"
+        exit 1
+    else
+        echo " Success"
 fi
--- test-simple-no-mocking.sh
+++ test-simple-no-mocking.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 ### Rather than roll my own, if I was really serious about bash script testing I'd dig further into https://github.com/sstephenson/bats
 
+printf "\n\nTESTING simple.sh\n"
+
+#setup spec
+outputFile=testing-simple-output.txt
+rm -f $outputFile
+sh ./simple.sh > $outputFile
+
+# --------------------
 printf "\nFound the correct number of rows\n"
 #given
 expectedRowCount=1
 
 #when
-sh ./simple.sh > testing-output.txt
-actualRowCount=(`wc testing-output.txt`)
+actualRowCount=(`wc $outputFile`)
 
 #then
 if [ $actualRowCount = $expectedRowCount ]
@@ -15,5 +22,19 @@ then
     echo " Success"
 else
     echo " FAILED: Row count was $actualRowCount not $expectedRowCount"
+    exit 1
+fi
+
+# --------------------
+printf "\nFound the correct row\n"
+#given
+row1="f2,2,num2002,more,stuff"
+
+#expects
+if grep -Fxq $row1 $outputFile
+then
+    echo " Success"
+else
+    echo " FAILED: Row $row1 was not found"
     exit 1
 fi
